6385a68e214d6d72d27d411e607ce924202367ea
Jonathan Niemi <jontniemi@gmail.com>
2023-05-12 18:01:43 -0400

    UIGame: Added right-click info menu
    -jenga blocks turn orange when focused/highlighted with a right click
    -focused block's info is displayed in a UI panel

--- Jenga-Game-Unity-Project/Assets/Scripts/Jenga/JengaBlock.cs
+++ Jenga-Game-Unity-Project/Assets/Scripts/Jenga/JengaBlock.cs
@@ -1,22 +1,35 @@
 namespace JengaGame
 {
     using UnityEngine;
+    using UnityEngine.Events;
 
     public class JengaBlock : MonoBehaviour
     {
+        public static UnityEvent<JengaBlock> OnRightClickedGlobal = new UnityEvent<JengaBlock>();
+
         [SerializeField] MasteryType masteryType = MasteryType.Glass;
         JengaBlockData data;
         BoxCollider _boxCollider;
         MeshRenderer _meshRenderer;
         Rigidbody _rigidbody;
+        Material _sharedMaterial;
 
         const float PADDING = 0.1f;
+        private static readonly Color SELECTED_COLOR = new Color(1, 0.5f, 0);
 
         void Awake()
         {
             _meshRenderer = GetComponent<MeshRenderer>();
+            _sharedMaterial = _meshRenderer.sharedMaterial;
             _boxCollider = GetComponent<BoxCollider>();
             _rigidbody = GetComponent<Rigidbody>();
+
+            OnRightClickedGlobal.AddListener(OnJengaBlockRightClickedGlobal);
+        }
+
+        void OnDestroy()
+        {
+            OnRightClickedGlobal.RemoveListener(OnJengaBlockRightClickedGlobal);
         }
 
         public void SetData(JengaBlockData newData)
@@ -24,10 +37,19 @@ namespace JengaGame
             data = newData;
         }
 
+        void OnJengaBlockRightClickedGlobal(JengaBlock block)
+        {
+            if (block == this)
+                _meshRenderer.material.color = SELECTED_COLOR;
+            else
+                _meshRenderer.material = _sharedMaterial;
+        }
+
         void OnMouseOver()
         {
             if (Input.GetMouseButton(1))
             {
+                OnRightClickedGlobal.Invoke(this);
                 Debug.Log(data.id);
                 // TODO: ui display showing info about this block
             }
@@ -52,6 +74,11 @@ namespace JengaGame
             if (!_meshRenderer)
                 _meshRenderer = GetComponent<MeshRenderer>();
             return (_meshRenderer.bounds.extents.z * 2) + PADDING;
+        }
+
+        public JengaBlockData GetData()
+        {
+            return data;
         }
 
         public MasteryType GetMasteryType()
